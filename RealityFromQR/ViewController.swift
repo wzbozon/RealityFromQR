6aa9cabbe66a32387861a20c88ece82f004f5dc4
Denis Kutlubaev <kutlubaev.denis@gmail.com>
2023-06-28 11:52:45 +0400

    Initial Commit

---
+++ RealityFromQR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RealityFromQR
+//
+//  Created by Denis Kutlubaev on 25/06/2023.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
