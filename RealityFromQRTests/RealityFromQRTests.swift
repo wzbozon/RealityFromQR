61783c0c65177429140df6378791fa0678de7c02
Denis Kutlubaev <kutlubaev.denis@gmail.com>
2023-08-13 10:19:39 +0400

    test: add test plan

--- RealityFromQRTests/RealityFromQRTests.swift
+++ RealityFromQRTests/RealityFromQRTests.swift
@@ -8,4 +8,21 @@
 import XCTest
 @testable import RealityFromQR
 
-final class RealityFromQRTests: XCTestCase {}
+final class RealityFromQRTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        let product = Product(id: 0, name: "Test", modelFileName: "test.usdz", imageName: "test.jpg")
+        guard let url = product.imageUrl?.absoluteString else {
+            XCTFail("No URL")
+            return
+        }
+        XCTAssertEqual(url, "http://ar.alwawee.ru/test.jpg")
+    }
+}
