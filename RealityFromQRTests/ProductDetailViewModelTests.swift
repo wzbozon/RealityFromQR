6ca749872a9069edae4c0302c89f7d6abc1ccf92
Denis Kutlubaev <kutlubaev.denis@gmail.com>
2023-08-27 11:00:19 +0400

    test: add ProductDetailViewModelTests, coverage 80%

---
+++ RealityFromQRTests/ProductDetailViewModelTests.swift
@@ -0,0 +1,29 @@
+//
+//  ProductDetailViewModelTests.swift
+//  RealityFromQRTests
+//
+//  Created by Denis Kutlubaev on 26/08/2023.
+//
+
+import XCTest
+@testable import RealityFromQR
+
+@MainActor
+final class ProductDetailViewModelTests: XCTestCase {
+
+    var product: Product!
+    var sut: ProductDetailViewModel!
+
+    override func setUp() async throws {
+        product = Product(id: 0, name: "", modelFileName: "", imageName: "soda.jpg")
+        sut = ProductDetailViewModel(product: product)
+    }
+
+    override func tearDown() async throws {}
+
+    func testFetchARModel() async {
+        try? await sut.fetchARModel()
+        XCTAssertFalse(product.isDownloading)
+    }
+
+}
